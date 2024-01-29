21768cdfc2bede506aab36464e8ab9a551f6c316
mahmoudashry98 <83143927+mahmoudashry98@users.noreply.github.com>
2024-01-29 23:21:15 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:doctor_app/core/routing/app_router.dart';
+import 'package:doctor_app/doc_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
+}
